10bf658f93804dce29f5c14409dc13395f26252e
lobamos <namosdjoro@gmail.com>
2018-01-17 21:46:27 +0100

    a

--- config.rb
+++ config.rb
@@ -1,19 +1,19 @@
 require 'twitter'
 
 client = Twitter::REST::Client.new do |config|
-	config.consumer_key = 's2AkXOH0CBYP2GVmNOek9eNmJ'
-	config.consumer_secret = 'bkFcApogIcIqeLxJpwXIm8qqFEmRzLmqAxGu16Xp2hci4hCBcC'
-	config.access_token  = '953649417390886913-z7kEHdcs37x1D5A50CTryFNIkdmI1dE'
-	config.access_token_secret = 'daHKTuIDTU31FrRvPQsakzOkpamyumyQxRMxidq2HVGOP'
+	config.consumer_key = ''
+	config.consumer_secret = ''
+	config.access_token  = ''
+	config.access_token_secret = ''
 end
-
-#client.update('bonjour monde!');
-=begin
+#envoie un tweet
+client.update('bonjour monde!');
+#stream tout les tweet avec coffe et tea dedans
 topics = ['coffee', 'tea']
 client.filter(track: topics.join(",")) do |object|
 	puts object.text if object.is_a?(Twitter::tweet)
 end
-=end
+
 #tweet a chaque journaliste entré dans l'array
 contact = ['@jcunniet', "#@PaulLampon", "@Aziliz31"]
 contact.each do |pseudo|
